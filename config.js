31a827ee92309c1d3a805a84f4444d98e1193ccf
Rafal Leszczynski <info@grafikart.pl>
2018-12-04 22:46:16 +0100

    25.7 Google_api

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '63085330155-9es65vip3tl4074am167gq7tiiesarmf.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'xdyPgOeOFrtsOBPHiWHLTnPY',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
